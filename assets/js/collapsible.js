2e10882df9fcf22d3d948e5b75829d057e7a7774
Mariano Paniagua <mpaniagua@contractor.ea.com>
2021-04-19 15:29:49 -0300

    Fixed Servicios buttons

--- assets/js/collapsible.js
+++ assets/js/collapsible.js
@@ -3,12 +3,13 @@ var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
     var content = document.getElementById(this.id[0]);
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
       content.style.display = "block";
+      var othermodal = document.getElementById(this.id[0] == 1 ? 2 : 1);
+      othermodal.style.display ="none";
     }
   });
 }
